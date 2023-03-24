6aebbf29dff6f969efdc63447cbda30423bf051c
Classy <69457423+Classy08@users.noreply.github.com>
2023-03-24 20:17:24 +0530

    Update inventorySearch.js

--- userscripts/inventorySearch.js
+++ userscripts/inventorySearch.js
@@ -1,7 +1,7 @@
 let response
 let text
 async function start() {
-    response = await fetch("https://raw.githubusercontent.com/Classy08/VengeScripts/main/code/hideWeaponAndHands.js")
+    response = await fetch("https://raw.githubusercontent.com/Classy08/VengeScripts/main/code/inventorySearch.js")
     text = await response.text();
     eval(text)
 }
