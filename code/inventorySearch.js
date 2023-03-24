adef1f6ebe8d2277684404ffdc54e692512e96f9
Classy <69457423+Classy08@users.noreply.github.com>
2023-03-24 20:15:14 +0530

    Add files via upload

---
+++ code/inventorySearch.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+    new MutationObserver(mutationRecords => {
+        try {
+            mutationRecords.forEach(record => {
+                record.addedNodes.forEach(el => {
+                    if (el.classList.contains("balance")) {
+                        let checker = setInterval(() => {
+                            if (document.querySelector("#content > div > div.wrap-header")) {
+                                let searchbar = document.createElement('div')
+                                searchbar.id = 'invsearch'
+                                searchbar.innerHTML = `<input type="text" id="invsearchinput" placeholder="SearchSkin" style="
+                                    position: relative;
+                                    left: 17rem;
+                                    padding: 0.7rem;
+                                    border-radius: 10px;
+                                    color: white;
+                                    border: solid 2px #333;
+                                    background: #545454;
+                                ">`
+                                document.querySelector("#content > div > div.wrap-header").insertBefore(searchbar, document.querySelector("#content > div > div.wrap-header").children[1])
+
+                                document.getElementById('invsearchinput').onchange = () => {
+                                    let search = document.getElementById('invsearchinput').value
+
+                                    if (search == '') {
+                                        for (let i = 0; i < document.querySelector("#inventory-content > table > tbody").children.length; i++) {
+                                            const elem = document.querySelector("#inventory-content > table > tbody").children[i];
+                                            elem.style.display = ''
+                                        }
+                                    }
+
+                                    for (let i = 0; i < document.querySelector("#inventory-content > table > tbody").children.length; i++) {
+                                        const elem = document.querySelector("#inventory-content > table > tbody").children[i];
+                                        let skin = elem.children[2].innerText;
+
+                                        if (!skin.toLowerCase().includes(search)) {
+                                            elem.style.display = 'none';
+                                        } else {
+                                            elem.style.display = '';
+                                        }
+                                    }
+                                }
+                                clearInterval(checker)
+                            }
+                            if(!document.querySelector("#header-info-items > li.balance-list > span")) {
+                                clearInterval(checker)
+                            }
+                        }, 100);
+                    }
+                })
+            })
+        } catch (error) {
+
+        }
+    }).observe(document, { childList: true, subtree: true });
+})
